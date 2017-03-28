54e97e2e7b646b8a1aa04fa06ecfcbf2b3f8dfed
inja2001m <dopa_1@naver.com>
2017-03-28 22:42:20 +0900

    Update Info

--- MapTool/MapTool/Resource.h
+++ MapTool/MapTool/Resource.h
@@ -8,14 +8,19 @@
 #define IDR_MapToolTYPE                 130
 #define IDD_FORMVIEW1                   310
 #define IDD_FORMVIEW2                   311
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1011
+#define IDC_BUTTON5                     1012
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           312
 #endif
 #endif
