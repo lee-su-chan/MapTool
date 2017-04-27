792452bd2553ba24f3409bfc7097cbfd430f3de5
inja2001m <dopa_1@naver.com>
2017-04-27 23:15:33 +0900

    Update MyResource h

---
+++ MapTool/MapTool/Directx/Resources/MyResource.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define MAIN_WND_WIDTH		1680
+#define MAIN_WND_HEIGHT		1024
+
+#define DIRECT_WND_WIDTH	1280
+#define DIRECT_WND_HEIGHT	1024
+
+
+struct TERRAIN_DESC
+{
+	int nCell;
+	int nTile;
+	char *baseTextureName;
+};
