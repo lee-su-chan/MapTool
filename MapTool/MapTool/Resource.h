4251066a5e9a78a7c78ae124761ec4da6e8b2e67
inja2001m <dopa_1@naver.com>
2017-04-27 23:12:53 +0900

    Add Info

--- MapTool/MapTool/Resource.h
+++ MapTool/MapTool/Resource.h
@@ -18,8 +18,17 @@
 #define IDC_STATIC1                     1017
 #define IDC_LIST3                       1021
 #define IDC_MFCSHELLLIST1               1023
-#define IDC_COMBO1                      1025
 #define IDC_COMBO2                      1026
+#define IDC_RADIO3                      1031
+#define IDC_RADIO4                      1036
+#define IDC_RADIO5                      1037
+#define IDC_RADIO6                      1038
+#define IDC_RADIO7                      1039
+#define IDC_RADIO8                      1040
+#define IDC_RADIO9                      1041
+#define IDC_RADIO10                     1042
+#define IDC_RADIO11                     1043
+#define IDC_RADIO12                     1044
 
 // Next default values for new objects
 // 
@@ -27,7 +36,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        314
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_CONTROL_VALUE         1032
 #define _APS_NEXT_SYMED_VALUE           313
 #endif
 #endif
