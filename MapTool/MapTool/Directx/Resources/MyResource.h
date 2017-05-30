7cc45e9f13e98d66b4e09674acfb181408b86a91
inja2001m <dopa_1@naver.com>
2017-05-30 16:17:12 +0900

    Update MyResource h

--- MapTool/MapTool/Directx/Resources/MyResource.h
+++ MapTool/MapTool/Directx/Resources/MyResource.h
@@ -1,15 +1,24 @@
 #pragma once
 
+#include <atlstr.h>
+#include <vector>
+
 #define MAIN_WND_WIDTH		1680
 #define MAIN_WND_HEIGHT		1024
 
 #define DIRECT_WND_WIDTH	1280
 #define DIRECT_WND_HEIGHT	1024
 
-
-struct TERRAIN_DESC
+namespace MyStruct
 {
-	int nCell;
-	int nTile;
-	char *baseTextureName;
-};
+	struct TERRAIN_DESC
+	{
+		int nCell;
+		int nTile;
+
+		std::vector<std::string> *textureNames;
+		int textureCurSel;	// Cursor Select
+		int nTexture;
+		char *baseTextureName;
+	};
+}
