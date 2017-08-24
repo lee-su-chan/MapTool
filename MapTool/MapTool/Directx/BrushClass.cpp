0ed1b39112e4a0a16b010b753901340492dceb2a
inja2001m <dopa_1@naver.com>
2017-08-24 23:58:17 +0900

    Create BrushClass

---
+++ MapTool/MapTool/Directx/BrushClass.cpp
@@ -0,0 +1,6 @@
+#include "BrushClass.h"
+
+BrushClass::BrushClass()
+{
+
+}
---
+++ MapTool/MapTool/Directx/BrushClass.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "D3D_Defines.h"
+
+class BrushClass
+{
+public:
+	BrushClass();
+	BrushClass(const BrushClass &) = default;
+	~BrushClass() = default;
+};
