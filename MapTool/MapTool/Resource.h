2c0d776c617f50aedfc193d6ccd835ab10e5c20c
inja2001m <dopa_1@naver.com>
2017-10-17 09:37:12 +0900

    Update Resource h

--- MapTool/MapTool/Resource.h
+++ MapTool/MapTool/Resource.h
@@ -6,8 +6,9 @@
 #define IDP_OLE_INIT_FAILED             100
 #define IDR_MAINFRAME                   128
 #define IDR_MapToolTYPE                 130
-#define IDD_FORMVIEW2                   311
+#define IDD_EDITVIEW                    311
 #define IDD_MAKENEWFILEDLG              312
+#define IDD_TOOLEDITVIEW                313
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
 #define IDC_BUTTON3                     1002
@@ -23,20 +24,28 @@
 #define IDC_RADIO4                      1036
 #define IDC_RADIO5                      1037
 #define IDC_RADIO6                      1038
+#define IDC_BUTTON6                     1038
 #define IDC_RADIO7                      1039
+#define IDC_BUTTON7                     1039
 #define IDC_RADIO8                      1040
+#define IDC_BUTTON8                     1040
 #define IDC_RADIO9                      1041
+#define IDC_BUTTON9                     1041
 #define IDC_RADIO10                     1042
+#define IDC_SLIDER1                     1042
 #define IDC_RADIO11                     1043
+#define IDC_RADIO1                      1043
 #define IDC_RADIO12                     1044
+#define IDC_RADIO2                      1044
+#define IDC_SPIN1                       1048
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_RESOURCE_VALUE        317
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1032
-#define _APS_NEXT_SYMED_VALUE           313
+#define _APS_NEXT_CONTROL_VALUE         1049
+#define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
